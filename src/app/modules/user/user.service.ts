181f5a77de63da82fa88dead3721ff4d2fe57f54
Naimur <naeemhasan28@gmail.com>
2023-12-27 02:00:38 +0600

    added getProfile service for users

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,19 @@
+import httpStatus from 'http-status'
+import { JwtPayload } from 'jsonwebtoken'
+import ApiError from '../../../errors/ApiError'
+import { IUser } from './user.interface'
+import { User } from './user.model'
+
+const getProfile = async (user: JwtPayload): Promise<IUser | null> => {
+  const profile = await User.findById(user.userId)
+
+  if (!profile) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found!')
+  }
+
+  return profile
+}
+
+export const UserService = {
+  getProfile,
+}
